7d3436a3461de805516ceac3484b07f389a88659
philip <kins.philip@gmail.com>
2026-02-14 12:55:54 +0200

    added model and mark to the cms

--- admin/controller/extension/module/ymm.php
+++ admin/controller/extension/module/ymm.php
@@ -69,8 +69,326 @@ class ControllerExtensionModuleYmm extends Controller {
         }
     }
 
-    // Placeholders for your sub-pages
-    public function makes() { die("Makes Page Working"); }
-    public function models() { die("Models Page Working"); }
-    public function years() { die("Years Page Working"); }
+
+    // ... existing install/index code ...
+
+    // ==========================================
+    // MAKES CONTROLLER LOGIC
+    // ==========================================
+    public function makes() {
+        $this->load->language('extension/module/ymm');
+        $this->document->setTitle('Manage Makes');
+        $this->load->model('extension/module/ymm');
+
+        $this->getList('make');
+    }
+
+    public function addMake() {
+        $this->load->language('extension/module/ymm');
+        $this->document->setTitle('Add Make');
+        $this->load->model('extension/module/ymm');
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
+            $this->model_extension_module_ymm->addMake($this->request->post);
+            $this->session->data['success'] = 'Success: You have modified makes!';
+            $this->response->redirect($this->url->link('extension/module/ymm/makes', 'user_token=' . $this->session->data['user_token'], true));
+        }
+
+        $this->getForm('make');
+    }
+
+    public function editMake() {
+        $this->load->language('extension/module/ymm');
+        $this->document->setTitle('Edit Make');
+        $this->load->model('extension/module/ymm');
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
+            $this->model_extension_module_ymm->editMake($this->request->get['make_id'], $this->request->post);
+            $this->session->data['success'] = 'Success: You have modified makes!';
+            $this->response->redirect($this->url->link('extension/module/ymm/makes', 'user_token=' . $this->session->data['user_token'], true));
+        }
+
+        $this->getForm('make');
+    }
+
+    public function deleteMake() {
+        $this->load->language('extension/module/ymm');
+        $this->document->setTitle('Delete Make');
+        $this->load->model('extension/module/ymm');
+
+        if (isset($this->request->post['selected']) && $this->validateDelete()) {
+            foreach ($this->request->post['selected'] as $make_id) {
+                $this->model_extension_module_ymm->deleteMake($make_id);
+            }
+            $this->session->data['success'] = 'Success: You have modified makes!';
+            $this->response->redirect($this->url->link('extension/module/ymm/makes', 'user_token=' . $this->session->data['user_token'], true));
+        }
+
+        $this->getList('make');
+    }
+
+    // ==========================================
+    // GENERIC HELPERS (REUSE FOR MODELS/YEARS)
+    // ==========================================
+    protected function getList($type) {
+        // 1. Initialize variables to prevent "Undefined variable" errors
+        $results = array();
+        $total = 0;
+        $data['heading_title'] = '';
+        $data['add'] = '';
+        $data['delete'] = '';
+
+        // 2. Setup standard page data
+        if (isset($this->request->get['page'])) {
+            $page = (int)$this->request->get['page'];
+        } else {
+            $page = 1;
+        }
+
+        $url = '';
+        if (isset($this->request->get['page'])) {
+            $url .= '&page=' . $this->request->get['page'];
+        }
+
+        // 3. Breadcrumbs
+        $data['breadcrumbs'] = array();
+        $data['breadcrumbs'][] = array(
+            'text' => 'Home',
+            'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+        );
+        $data['breadcrumbs'][] = array(
+            'text' => 'YMM Module',
+            'href' => $this->url->link('extension/module/ymm', 'user_token=' . $this->session->data['user_token'], true)
+        );
+
+        // 4. LOGIC SWITCHER
+        $filter_data = array(
+            'start' => ($page - 1) * 20,
+            'limit' => 20
+        );
+
+        // ==========================
+        // CASE: MAKE
+        // ==========================
+        if ($type == 'make') {
+            $data['heading_title'] = 'Manage Makes';
+            $data['add'] = $this->url->link('extension/module/ymm/addMake', 'user_token=' . $this->session->data['user_token'] . $url, true);
+            $data['delete'] = $this->url->link('extension/module/ymm/deleteMake', 'user_token=' . $this->session->data['user_token'] . $url, true);
+            
+            $results = $this->model_extension_module_ymm->getMakes($filter_data);
+            $total = $this->model_extension_module_ymm->getTotalMakes();
+        } 
+        // ==========================
+        // CASE: MODEL (This was missing!)
+        // ==========================
+        elseif ($type == 'model') {
+            $data['heading_title'] = 'Manage Models';
+            $data['add'] = $this->url->link('extension/module/ymm/addModel', 'user_token=' . $this->session->data['user_token'] . $url, true);
+            $data['delete'] = $this->url->link('extension/module/ymm/deleteModel', 'user_token=' . $this->session->data['user_token'] . $url, true);
+
+            // You must have getModels() in your admin/model/extension/module/ymm.php
+            $results = $this->model_extension_module_ymm->getModels($filter_data);
+            $total = $this->model_extension_module_ymm->getTotalModels();
+        }
+        // ==========================
+        // CASE: YEAR (Placeholder)
+        // ==========================
+        elseif ($type == 'year') {
+            $data['heading_title'] = 'Manage Years';
+            $data['add'] = $this->url->link('extension/module/ymm/addYear', 'user_token=' . $this->session->data['user_token'] . $url, true);
+            $data['delete'] = $this->url->link('extension/module/ymm/deleteYear', 'user_token=' . $this->session->data['user_token'] . $url, true);
+
+            $results = $this->model_extension_module_ymm->getYears($filter_data);
+            $total = $this->model_extension_module_ymm->getTotalYears();
+        }
+
+        // 5. Process Results into View Array
+        $data['items'] = array();
+        
+        if ($results) {
+            foreach ($results as $result) {
+                $edit_link = $this->url->link('extension/module/ymm/edit' . ucfirst($type), 'user_token=' . $this->session->data['user_token'] . '&' . $type . '_id=' . $result[$type . '_id'] . $url, true);
+                
+                // Handle different data fields safely
+                $make_name = isset($result['make_name']) ? $result['make_name'] : '';
+                $model_name = isset($result['model_name']) ? $result['model_name'] : '';
+                $year_start = isset($result['year_start']) ? $result['year_start'] : '';
+                $year_end   = isset($result['year_end']) ? $result['year_end'] : '';
+
+                $data['items'][] = array(
+                    'id'         => $result[$type . '_id'],
+                    'name'       => $result['name'],
+                    'make_name'  => $make_name,  // Only used for models/years
+                    'model_name' => $model_name, // Only used for years
+                    'year_start' => $year_start, // Only used for years
+                    'year_end'   => $year_end,   // Only used for years
+                    'edit'       => $edit_link
+                );
+            }
+        }
+
+        // 6. Final Data for View
+        $data['user_token'] = $this->session->data['user_token'];
+        
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
+        } else {
+            $data['error_warning'] = '';
+        }
+
+        if (isset($this->session->data['success'])) {
+            $data['success'] = $this->session->data['success'];
+            unset($this->session->data['success']);
+        } else {
+            $data['success'] = '';
+        }
+
+        $data['header'] = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
+
+        // Pagination
+        $pagination = new Pagination();
+        $pagination->total = $total;
+        $pagination->page = $page;
+        $pagination->limit = 20;
+        $pagination->url = $this->url->link('extension/module/ymm/' . $type . 's', 'user_token=' . $this->session->data['user_token'] . $url . '&page={page}', true);
+        $data['pagination'] = $pagination->render();
+        
+        $data['results'] = sprintf('Showing %d to %d of %d (%d Pages)', ($total) ? (($page - 1) * 20) + 1 : 0, ((($page - 1) * 20) > ($total - 20)) ? $total : ((($page - 1) * 20) + 20), $total, ceil($total / 20));
+
+        // Load specific view based on type
+        $this->response->setOutput($this->load->view('extension/module/ymm_' . $type . '_list', $data));
+    }
+
+    protected function getForm($type) {
+        $data['text_form'] = !isset($this->request->get[$type . '_id']) ? 'Add ' . ucfirst($type) : 'Edit ' . ucfirst($type);
+        
+        if (isset($this->request->get[$type . '_id'])) {
+            $data['action'] = $this->url->link('extension/module/ymm/edit' . ucfirst($type), 'user_token=' . $this->session->data['user_token'] . '&' . $type . '_id=' . $this->request->get[$type . '_id'], true);
+        } else {
+            $data['action'] = $this->url->link('extension/module/ymm/add' . ucfirst($type), 'user_token=' . $this->session->data['user_token'], true);
+        }
+        
+        $data['cancel'] = $this->url->link('extension/module/ymm/' . $type . 's', 'user_token=' . $this->session->data['user_token'], true);
+
+        // Get Data
+        if (isset($this->request->get[$type . '_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+            $funcName = 'get' . ucfirst($type);
+            $info = $this->model_extension_module_ymm->$funcName($this->request->get[$type . '_id']);
+        }
+
+        $data['name'] = isset($this->request->post['name']) ? $this->request->post['name'] : (!empty($info) ? $info['name'] : '');
+
+        // If Type is Model, load Makes for dropdown
+        if ($type == 'model') {
+            $data['makes'] = $this->model_extension_module_ymm->getMakes();
+            $data['make_id'] = isset($this->request->post['make_id']) ? $this->request->post['make_id'] : (!empty($info) ? $info['make_id'] : '');
+        }
+
+        $data['user_token'] = $this->session->data['user_token'];
+        $data['header'] = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
+
+        $this->response->setOutput($this->load->view('extension/module/ymm_' . $type . '_form', $data));
+    }
+
+    protected function validateForm() {
+        if (!$this->user->hasPermission('modify', 'extension/module/ymm')) {
+            $this->error['warning'] = 'Warning: You do not have permission to modify this module!';
+        }
+        return !$this->error;
+    }
+
+    protected function validateDelete() {
+        if (!$this->user->hasPermission('modify', 'extension/module/ymm')) {
+            $this->error['warning'] = 'Warning: You do not have permission to modify this module!';
+        }
+        return !$this->error;
+    }
+
+
+    // ==========================================
+    // MODELS CONTROLLER LOGIC
+    // ==========================================
+    public function models() {
+        $this->load->language('extension/module/ymm');
+        $this->document->setTitle('Manage Models');
+        $this->load->model('extension/module/ymm');
+        $this->getList('model');
+    }
+
+    public function addModel() {
+        $this->load->language('extension/module/ymm');
+        $this->document->setTitle('Add Model');
+        $this->load->model('extension/module/ymm');
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
+            $this->model_extension_module_ymm->addModel($this->request->post);
+            $this->session->data['success'] = 'Success: You have modified models!';
+            $this->response->redirect($this->url->link('extension/module/ymm/models', 'user_token=' . $this->session->data['user_token'], true));
+        }
+
+        $this->getForm('model');
+    }
+
+    public function editModel() {
+        $this->load->language('extension/module/ymm');
+        $this->document->setTitle('Edit Model');
+        $this->load->model('extension/module/ymm');
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
+            $this->model_extension_module_ymm->editModel($this->request->get['model_id'], $this->request->post);
+            $this->session->data['success'] = 'Success: You have modified models!';
+            $this->response->redirect($this->url->link('extension/module/ymm/models', 'user_token=' . $this->session->data['user_token'], true));
+        }
+
+        $this->getForm('model');
+    }
+
+    public function deleteModel() {
+        $this->load->language('extension/module/ymm');
+        $this->document->setTitle('Delete Model');
+        $this->load->model('extension/module/ymm');
+
+        if (isset($this->request->post['selected']) && $this->validateDelete()) {
+            foreach ($this->request->post['selected'] as $model_id) {
+                $this->model_extension_module_ymm->deleteModel($model_id);
+            }
+            $this->session->data['success'] = 'Success: You have modified models!';
+            $this->response->redirect($this->url->link('extension/module/ymm/models', 'user_token=' . $this->session->data['user_token'], true));
+        }
+
+        $this->getList('model');
+    }
+
+    public function autocomplete() {
+        $json = array();
+
+        if (isset($this->request->get['filter_name'])) {
+            $this->load->model('extension/module/ymm');
+
+            $filter_data = array(
+                'filter_name' => $this->request->get['filter_name'],
+                'start'       => 0,
+                'limit'       => 10 // Show top 10 results
+            );
+
+            $results = $this->model_extension_module_ymm->getModels($filter_data);
+
+            foreach ($results as $result) {
+                // FORMAT: Make > Model
+                $json[] = array(
+                    'model_id' => $result['model_id'],
+                    'name'     => strip_tags(html_entity_decode($result['make_name'] . ' > ' . $result['name'], ENT_QUOTES, 'UTF-8'))
+                );
+            }
+        }
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+    
+
 }
---
+++ admin/model/extension/module/ymm.php
@@ -0,0 +1,150 @@
+<?php
+class ModelExtensionModuleYmm extends Model {
+    
+    // ==========================
+    // MAKES
+    // ==========================
+    public function addMake($data) {
+        $this->db->query("INSERT INTO " . DB_PREFIX . "ymm_makes SET name = '" . $this->db->escape($data['name']) . "'");
+    }
+
+    public function editMake($make_id, $data) {
+        $this->db->query("UPDATE " . DB_PREFIX . "ymm_makes SET name = '" . $this->db->escape($data['name']) . "' WHERE make_id = '" . (int)$make_id . "'");
+    }
+
+    public function deleteMake($make_id) {
+        $this->db->query("DELETE FROM " . DB_PREFIX . "ymm_makes WHERE make_id = '" . (int)$make_id . "'");
+        // Optional: Delete related models?
+    }
+
+    public function getMake($make_id) {
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "ymm_makes WHERE make_id = '" . (int)$make_id . "'");
+        return $query->row;
+    }
+
+    public function getMakes($data = array()) {
+        $sql = "SELECT * FROM " . DB_PREFIX . "ymm_makes";
+        if (!empty($data['filter_name'])) {
+            $sql .= " WHERE name LIKE '" . $this->db->escape($data['filter_name']) . "%'";
+        }
+        $sql .= " ORDER BY name ASC";
+        
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) $data['start'] = 0;
+            if ($data['limit'] < 1) $data['limit'] = 20;
+            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+        }
+        
+        $query = $this->db->query($sql);
+        return $query->rows;
+    }
+
+    public function getTotalMakes() {
+        $query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "ymm_makes");
+        return $query->row['total'];
+    }
+
+    // ==========================
+    // MODELS
+    // ==========================
+    public function addModel($data) {
+        $this->db->query("INSERT INTO " . DB_PREFIX . "ymm_models SET make_id = '" . (int)$data['make_id'] . "', name = '" . $this->db->escape($data['name']) . "'");
+    }
+
+    public function editModel($model_id, $data) {
+        $this->db->query("UPDATE " . DB_PREFIX . "ymm_models SET make_id = '" . (int)$data['make_id'] . "', name = '" . $this->db->escape($data['name']) . "' WHERE model_id = '" . (int)$model_id . "'");
+    }
+
+    public function deleteModel($model_id) {
+        $this->db->query("DELETE FROM " . DB_PREFIX . "ymm_models WHERE model_id = '" . (int)$model_id . "'");
+    }
+
+    public function getModel($model_id) {
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "ymm_models WHERE model_id = '" . (int)$model_id . "'");
+        return $query->row;
+    }
+
+    public function getModels($data = array()) {
+        $sql = "SELECT m.*, mk.name as make_name FROM " . DB_PREFIX . "ymm_models m LEFT JOIN " . DB_PREFIX . "ymm_makes mk ON (m.make_id = mk.make_id)";
+        $sql .= " ORDER BY m.name ASC";
+        
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) $data['start'] = 0;
+            if ($data['limit'] < 1) $data['limit'] = 20;
+            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+        }
+        
+        $query = $this->db->query($sql);
+        return $query->rows;
+    }
+
+    public function getTotalModels() {
+        $query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "ymm_models");
+        return $query->row['total'];
+    }
+
+    // ==========================
+    // YEARS
+    // ==========================
+    public function addYear($data) {
+        $this->db->query("INSERT INTO " . DB_PREFIX . "ymm_year SET model_id = '" . (int)$data['model_id'] . "', name = '" . $this->db->escape($data['name']) . "', year_start = '" . (int)$data['year_start'] . "', year_end = '" . (int)$data['year_end'] . "'");
+    }
+
+    public function editYear($year_id, $data) {
+        $this->db->query("UPDATE " . DB_PREFIX . "ymm_year SET model_id = '" . (int)$data['model_id'] . "', name = '" . $this->db->escape($data['name']) . "', year_start = '" . (int)$data['year_start'] . "', year_end = '" . (int)$data['year_end'] . "' WHERE year_id = '" . (int)$year_id . "'");
+    }
+
+    public function deleteYear($year_id) {
+        $this->db->query("DELETE FROM " . DB_PREFIX . "ymm_year WHERE year_id = '" . (int)$year_id . "'");
+    }
+
+    public function getYear($year_id) {
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "ymm_year WHERE year_id = '" . (int)$year_id . "'");
+        return $query->row;
+    }
+
+    public function getYears($data = array()) {
+        // Join with Model and Make so we can show "Make > Model > Year"
+        $sql = "SELECT y.*, m.name as model_name, mk.name as make_name 
+                FROM " . DB_PREFIX . "ymm_year y 
+                LEFT JOIN " . DB_PREFIX . "ymm_models m ON (y.model_id = m.model_id) 
+                LEFT JOIN " . DB_PREFIX . "ymm_makes mk ON (m.make_id = mk.make_id)";
+        
+        $sql .= " ORDER BY y.year_start DESC";
+
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) $data['start'] = 0;
+            if ($data['limit'] < 1) $data['limit'] = 20;
+            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+        }
+
+        $query = $this->db->query($sql);
+        return $query->rows;
+    }
+
+    public function getTotalYears() {
+        $query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "ymm_year");
+        return $query->row['total'];
+    }
+
+    // public function getModels($data = array()) {
+    //     $sql = "SELECT m.*, mk.name as make_name FROM " . DB_PREFIX . "ymm_models m LEFT JOIN " . DB_PREFIX . "ymm_makes mk ON (m.make_id = mk.make_id)";
+        
+    //     // NEW: Search Logic
+    //     if (!empty($data['filter_name'])) {
+    //         // Search either Model Name OR Make Name
+    //         $sql .= " WHERE m.name LIKE '" . $this->db->escape($data['filter_name']) . "%' OR mk.name LIKE '" . $this->db->escape($data['filter_name']) . "%'";
+    //     }
+        
+    //     $sql .= " ORDER BY mk.name ASC, m.name ASC"; // Sort by Make, then Model
+        
+    //     if (isset($data['start']) || isset($data['limit'])) {
+    //         if ($data['start'] < 0) $data['start'] = 0;
+    //         if ($data['limit'] < 1) $data['limit'] = 20;
+    //         $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+    //     }
+        
+    //     $query = $this->db->query($sql);
+    //     return $query->rows;
+    // }
+}

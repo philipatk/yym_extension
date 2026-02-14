4a574976e0159441f957e36623c2b51cc3fd38b6
philip <kins.philip@gmail.com>
2026-02-14 12:16:37 +0200

    fixed bug for instolation of exention ymm

--- admin/controller/extension/module/ymm.php
+++ admin/controller/extension/module/ymm.php
@@ -2,53 +2,12 @@
 class ControllerExtensionModuleYmm extends Controller {
     private $error = array();
 
+    // 1. The Settings Page (Fixes your "Page Not Found" error)
     public function index() {
         $this->load->language('extension/module/ymm');
-
         $this->document->setTitle($this->language->get('heading_title'));
 
-        $this->load->model('setting/setting');
-
-        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-            $this->model_setting_setting->editSetting('module_ymm', $this->request->post);
-
-            $this->session->data['success'] = $this->language->get('text_success');
-
-            $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
-        }
-
-        if (isset($this->error['warning'])) {
-            $data['error_warning'] = $this->error['warning'];
-        } else {
-            $data['error_warning'] = '';
-        }
-
-        $data['breadcrumbs'] = array();
-
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_home'),
-            'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
-        );
-
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_extension'),
-            'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true)
-        );
-
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('extension/module/ymm', 'user_token=' . $this->session->data['user_token'], true)
-        );
-
-        $data['action'] = $this->url->link('extension/module/ymm', 'user_token=' . $this->session->data['user_token'], true);
-        $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
-
-        if (isset($this->request->post['module_ymm_status'])) {
-            $data['module_ymm_status'] = $this->request->post['module_ymm_status'];
-        } else {
-            $data['module_ymm_status'] = $this->config->get('module_ymm_status');
-        }
-
+        // Basic view logic so the page loads
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['footer'] = $this->load->controller('common/footer');
@@ -56,11 +15,62 @@ class ControllerExtensionModuleYmm extends Controller {
         $this->response->setOutput($this->load->view('extension/module/ymm', $data));
     }
 
-    protected function validate() {
-        if (!$this->user->hasPermission('modify', 'extension/module/ymm')) {
-            $this->error['warning'] = $this->language->get('error_permission');
+    public function install() {
+        // 1. Load the Settings Model
+        $this->load->model('setting/setting');
+
+        // 2. Force the module to be "Enabled" immediately
+        // "module_ymm_status" is the standard key OpenCart looks for.
+        $this->model_setting_setting->editSetting('module_ymm', ['module_ymm_status' => 1]);
+
+        // 3. Your existing event code for the menu...
+        $this->load->model('setting/event');
+        $this->model_setting_event->deleteEventByCode('mmy_menu_injection');
+        $this->model_setting_event->addEvent('mmy_menu_injection', 'admin/view/common/column_left/before', 'extension/module/ymm/addMenu');
+    }
+
+    public function uninstall() {
+        $this->load->model('setting/event');
+        $this->model_setting_event->deleteEventByCode('mmy_menu_injection');
+    }
+
+    // 3. Insert the menu item
+    public function addMenu(&$route, &$data) {
+        $mmy = array();
+        $user_token = $this->session->data['user_token'];
+
+        // FIXED: Points to functions inside THIS file (makes, models, years)
+        if ($this->user->hasPermission('access', 'extension/module/ymm')) {
+            $mmy[] = array(
+                'name'     => 'Makes',
+                'href'     => $this->url->link('extension/module/ymm/makes', 'user_token=' . $user_token, true),
+                'children' => array()
+            );
+            $mmy[] = array(
+                'name'     => 'Models',
+                'href'     => $this->url->link('extension/module/ymm/models', 'user_token=' . $user_token, true),
+                'children' => array()
+            );
+            $mmy[] = array(
+                'name'     => 'Years',
+                'href'     => $this->url->link('extension/module/ymm/years', 'user_token=' . $user_token, true),
+                'children' => array()
+            );
         }
 
-        return !$this->error;
+        if ($mmy) {
+            $data['menus'][] = array(
+                'id'       => 'menu-mmy',
+                'icon'     => 'fa-car',
+                'name'     => 'Vehicle Filter',
+                'href'     => '',
+                'children' => $mmy
+            );
+        }
     }
+
+    // Placeholders for your sub-pages
+    public function makes() { die("Makes Page Working"); }
+    public function models() { die("Models Page Working"); }
+    public function years() { die("Years Page Working"); }
 }
--- admin/language/en-gb/extension/module/ymm.php
+++ admin/language/en-gb/extension/module/ymm.php
@@ -1,14 +1,7 @@
 <?php
 // Heading
-$_['heading_title']    = 'Find You Product';
+$_['heading_title']    = 'YMM Filter';
 
 // Text
 $_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Success: You have modified YMM Filter module!';
-$_['text_edit']        = 'Edit YMM Filter Module';
-
-// Entry
-$_['entry_status']     = 'Status';
-
-// Error
-$_['error_permission'] = 'Warning: You do not have permission to modify YMM Filter module!';
